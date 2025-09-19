f9ddb06ba88ea06e109c55035d32b86584d5b84e
Thanawat <thanawat.nueang@gmail.com>
2025-09-19 19:07:11 +0700

    chore: reset project to clean state (keep README only)

--- backend/migrations/20250908154027-rename-wrong-table.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-export default {
-  async up(queryInterface) {
-    await queryInterface.renameTable('eating_histories', 'eating_history'); // แก้ตามที่ผิด/ถูกจริง
-  },
-  async down(queryInterface) {
-    await queryInterface.renameTable('eating_histories', 'eating_history');
-  }
-};
