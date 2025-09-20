ab1190ce0f4825ea113d180fa2a3762844eb39cc
Thanawat Nueangchumphon <thanawat.nueang@gmail.com>
2025-09-20 13:28:47 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,12 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (_req, res) => {
-  res.send('Hello from Azure ✅');
-});
-
-// health endpoint สำหรับ probe
-app.get('/healthz', (_req, res) => {
-  res.status(200).send('ok');
+app.get("/", (req, res) => {
+  res.send("Hello from Azure ✅");
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${port}`);
 });
