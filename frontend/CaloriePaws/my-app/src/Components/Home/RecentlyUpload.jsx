f5e05a6aafc8a26cf77d3326d8bb9ea9413259d5
Thanawat <thanawat.nueang@gmail.com>
2025-09-21 15:24:55 +0700

    PUSH frontend

--- frontend/CaloriePaws/my-app/src/Components/Home/RecentlyUpload.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const RecentlyUpload = () => {
-  return (
-    <div></div>
-  )
-}
--- frontend/CaloriePaws/my-app/src/Components/NavBar/CalorieToday.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const CalorieToday = () => {
-  return (
-    <div>CalorieToday</div>
-  )
-}
